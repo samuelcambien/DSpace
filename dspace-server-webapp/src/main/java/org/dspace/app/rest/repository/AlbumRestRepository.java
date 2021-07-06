6819f4739221931554510ed0b955566061518781
Samuel <samuelcambien@gmail.com>
2021-07-06 19:11:24 +0200

    Update endpoint in Repository

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/AlbumRestRepository.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/AlbumRestRepository.java
@@ -7,11 +7,15 @@
  */
 package org.dspace.app.rest.repository;
 
+import static org.apache.commons.lang.StringUtils.isBlank;
 import static org.dspace.app.rest.converter.AlbumConverter.parseDate;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.dspace.app.rest.converter.ConverterService;
 import org.dspace.app.rest.exception.UnprocessableEntityException;
@@ -22,6 +26,7 @@ import org.dspace.music.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
 
@@ -77,6 +82,37 @@ public class AlbumRestRepository extends DSpaceRestRepository<AlbumRest, UUID> {
 
         Album album = albumService.create(context, albumRest.getTitle(), albumRest.getArtist());
         album.setReleaseDate(parseDate(albumRest.getReleaseDate()));
+        return converter.toRest(album, utils.obtainProjection());
+    }
+
+    @Override
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public AlbumRest put(Context context, HttpServletRequest request, String apiCategory, String model, UUID id,
+                         JsonNode jsonNode) {
+
+        AlbumRest albumRest;
+        try {
+            albumRest = new ObjectMapper().readValue(jsonNode.toString(), AlbumRest.class);
+        } catch (IOException e) {
+            throw new UnprocessableEntityException("Error parsing album: " + e.getMessage());
+        }
+
+        if (isBlank(albumRest.getTitle())) {
+            throw new UnprocessableEntityException("Required property missing: album title");
+        }
+
+        if (isBlank(albumRest.getArtist())) {
+            throw new UnprocessableEntityException("Required property missing: album artist");
+        }
+
+        Album album = albumService.find(context, id).orElseThrow(
+            () -> new ResourceNotFoundException(apiCategory + "." + model + " with id: " + id + " not found")
+        );
+
+        album.setArtist(albumRest.getArtist());
+        album.setTitle(albumRest.getTitle());
+        album.setReleaseDate(parseDate(albumRest.getReleaseDate()));
+
         return converter.toRest(album, utils.obtainProjection());
     }
 
