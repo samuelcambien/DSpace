ba22775f9aea8f36743f85a8677ebb53f0e1398b
Samuel <samuelcambien@gmail.com>
2021-07-07 13:13:32 +0200

    ITs for CUD + Patch

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/AlbumRestRepositoryIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/AlbumRestRepositoryIT.java
@@ -7,6 +7,9 @@
  */
 package org.dspace.app.rest;
 
+import static java.util.Arrays.asList;
+import static java.util.UUID.randomUUID;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON_PATCH_JSON;
 import static org.dspace.app.rest.matcher.AlbumMatcher.matchAlbum;
 import static org.dspace.app.rest.model.AlbumRest.DATE_FORMAT;
 import static org.dspace.builder.AlbumBuilder.createAlbum;
@@ -14,11 +17,21 @@ import static org.dspace.builder.CollectionBuilder.createCollection;
 import static org.dspace.builder.CommunityBuilder.createCommunity;
 import static org.dspace.builder.ItemBuilder.createItem;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.dspace.app.rest.model.AlbumRest;
+import org.dspace.app.rest.model.patch.AddOperation;
+import org.dspace.app.rest.model.patch.ReplaceOperation;
 import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
 import org.dspace.content.Collection;
 import org.dspace.content.Community;
@@ -119,5 +132,267 @@ public class AlbumRestRepositoryIT extends AbstractControllerIntegrationTest {
                 .perform(get("/api/music/albums/" + album1.getID() + "/item"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(item.getID() + "")));
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        AlbumRest albumRest = new AlbumRest();
+        albumRest.setTitle("test title");
+        albumRest.setArtist("test artist");
+
+        String authToken = getAuthToken(admin.getEmail(), password);
+        getClient(authToken)
+                .perform(post("/api/music/albums/")
+                        .content(mapper.writeValueAsBytes(albumRest))
+                                .contentType(contentType))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(not(empty()))))
+                .andExpect(jsonPath("$.title", is("test title")))
+                .andExpect(jsonPath("$.artist", is("test artist")));
+    }
+
+    @Test
+    public void testCreateUnauthorized() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        AlbumRest albumRest = new AlbumRest();
+        albumRest.setTitle("test title");
+        albumRest.setArtist("test artist");
+
+        getClient()
+                .perform(post("/api/music/albums/")
+                        .content(mapper.writeValueAsBytes(albumRest))
+                                .contentType(contentType))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void testCreateForbidden() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        AlbumRest albumRest = new AlbumRest();
+        albumRest.setTitle("test title");
+        albumRest.setArtist("test artist");
+
+        String authToken = getAuthToken(eperson.getEmail(), password);
+        getClient(authToken)
+                .perform(post("/api/music/albums/")
+                        .content(mapper.writeValueAsBytes(albumRest))
+                                .contentType(contentType))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void testCreateUnprocessableEntity() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        AlbumRest albumRest = new AlbumRest();
+        albumRest.setArtist("test artist");
+
+        String authToken = getAuthToken(admin.getEmail(), password);
+        getClient(authToken)
+                .perform(post("/api/music/albums/")
+                        .content(mapper.writeValueAsBytes(albumRest))
+                                .contentType(contentType))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        AlbumRest albumRest = new AlbumRest();
+        albumRest.setTitle("test title updated");
+        albumRest.setArtist("test artist updated");
+
+        String authToken = getAuthToken(admin.getEmail(), password);
+        getClient(authToken)
+                .perform(put("/api/music/albums/" + album1.getID())
+                        .content(mapper.writeValueAsBytes(albumRest))
+                        .contentType(contentType))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(album1.getID() + "")))
+                .andExpect(jsonPath("$.title", is("test title updated")))
+                .andExpect(jsonPath("$.artist", is("test artist updated")));
+    }
+
+    @Test
+    public void testUpdateUnauthorized() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        AlbumRest albumRest = new AlbumRest();
+        albumRest.setTitle("test title updated");
+        albumRest.setArtist("test artist updated");
+
+        getClient()
+                .perform(put("/api/music/albums/" + album1.getID())
+                        .content(mapper.writeValueAsBytes(albumRest))
+                        .contentType(contentType))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void testUpdateForbidden() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        AlbumRest albumRest = new AlbumRest();
+        albumRest.setTitle("test title updated");
+        albumRest.setArtist("test artist updated");
+
+        String authToken = getAuthToken(eperson.getEmail(), password);
+        getClient(authToken)
+                .perform(put("/api/music/albums/" + album1.getID())
+                        .content(mapper.writeValueAsBytes(albumRest))
+                        .contentType(contentType))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void testUpdateNotFound() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        AlbumRest albumRest = new AlbumRest();
+        albumRest.setTitle("test title updated");
+        albumRest.setArtist("test artist updated");
+
+        String authToken = getAuthToken(admin.getEmail(), password);
+        getClient(authToken)
+                .perform(put("/api/music/albums/" + randomUUID())
+                        .content(mapper.writeValueAsBytes(albumRest))
+                        .contentType(contentType))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testUpdateUnprocessableEntity() throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        AlbumRest albumRest = new AlbumRest();
+        albumRest.setTitle("test title updated");
+
+        String authToken = getAuthToken(admin.getEmail(), password);
+        getClient(authToken)
+                .perform(put("/api/music/albums/" + album1.getID())
+                        .content(mapper.writeValueAsBytes(albumRest))
+                        .contentType(contentType))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    public void testPatch() throws Exception {
+
+        String patchBody = getPatchContent(asList(
+                new ReplaceOperation("/title", "test title patched"),
+                new ReplaceOperation("/artist", "test artist patched")
+        ));
+
+        String authToken = getAuthToken(admin.getEmail(), password);
+        getClient(authToken)
+                .perform(patch("/api/music/albums/" + album1.getID())
+                        .content(patchBody)
+                        .contentType(APPLICATION_JSON_PATCH_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(album1.getID() + "")))
+                .andExpect(jsonPath("$.title", is("test title patched")))
+                .andExpect(jsonPath("$.artist", is("test artist patched")));
+    }
+
+    @Test
+    public void testPatchUnauthorized() throws Exception {
+
+        String patchBody = getPatchContent(asList(
+                new ReplaceOperation("/title", "test title patched"),
+                new ReplaceOperation("/artist", "test artist patched")
+        ));
+
+        getClient()
+                .perform(patch("/api/music/albums/" + album1.getID())
+                        .content(patchBody)
+                        .contentType(APPLICATION_JSON_PATCH_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void testPatchForbidden() throws Exception {
+
+        String patchBody = getPatchContent(asList(
+                new ReplaceOperation("/title", "test title patched"),
+                new ReplaceOperation("/artist", "test artist patched")
+        ));
+
+        String authToken = getAuthToken(eperson.getEmail(), password);
+        getClient(authToken)
+                .perform(patch("/api/music/albums/" + album1.getID())
+                        .content(patchBody)
+                        .contentType(APPLICATION_JSON_PATCH_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void testPatchNotFound() throws Exception {
+
+        String patchBody = getPatchContent(asList(
+                new ReplaceOperation("/title", "test title patched"),
+                new ReplaceOperation("/artist", "test artist patched")
+        ));
+
+        String authToken = getAuthToken(admin.getEmail(), password);
+        getClient(authToken)
+                .perform(patch("/api/music/albums/" + randomUUID())
+                        .content(patchBody)
+                        .contentType(APPLICATION_JSON_PATCH_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testPatchBadRequest() throws Exception {
+
+        String patchBody = getPatchContent(asList(
+                new AddOperation("/title", "test title patched")
+        ));
+
+        String authToken = getAuthToken(admin.getEmail(), password);
+        getClient(authToken)
+                .perform(patch("/api/music/albums/" + album1.getID())
+                        .content(patchBody)
+                        .contentType(APPLICATION_JSON_PATCH_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+        String authToken = getAuthToken(admin.getEmail(), password);
+        getClient(authToken)
+                .perform(delete("/api/music/albums/" + album1.getID()))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void testDeleteUnauthorized() throws Exception {
+
+        getClient()
+                .perform(delete("/api/music/albums/" + album1.getID()))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void testDeleteForbidden() throws Exception {
+
+        String authToken = getAuthToken(eperson.getEmail(), password);
+        getClient(authToken)
+                .perform(delete("/api/music/albums/" + album1.getID()))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void testDeleteNotFound() throws Exception {
+
+        String authToken = getAuthToken(admin.getEmail(), password);
+        getClient(authToken)
+                .perform(delete("/api/music/albums/" + randomUUID()))
+                .andExpect(status().isNotFound());
     }
 }
